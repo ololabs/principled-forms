2d34455b77ce72aa1acfd2bd51b80c7b126f21b0
Chris Krycho <chris@chriskrycho.com>
2018-04-22 10:37:07 -0600

    Build out some of field component.

--- packages/ember-principled-forms/addon/components/principled-form-field/component.ts
+++ packages/ember-principled-forms/addon/components/principled-form-field/component.ts
@@ -1,13 +1,43 @@
 import Component from '@ember/component';
+import { assert } from '@ember/debug';
+import { action } from '@ember-decorators/object';
 
-import Field from 'principled-forms/field';
+import { defaultTo, noop, isString, isObject } from 'lodash';
+import Field, { InputTypes } from 'principled-forms/field';
 
 // @ts-ignore: Ignore import of compiled template
 import layout from './template';
 
-export default class PrincipledFormField extends Component {
+export default class PrincipledFormField<T extends InputTypes> extends Component {
   layout = layout;
 
   // normal class body definition here
+  label: string;
+  model: Field<T>;
+
+  onChange?: (value: T) => void = defaultTo(this.onChange, noop);
+  onInput?: (value: T) => void = defaultTo(this.onInput, noop);
+
   name: string = this.name || 'principled-form-field';
+
+  constructor() {
+    super();
+
+    assert('`label` is required', isString(this.label));
+    assert('`model` is required', isObject(this.model));
+    assert(
+      'at least one of `onChange`, `onInput` is required',
+      this.onChange !== noop || this.onInput !== noop
+    );
+  }
+
+  @action
+  change(value: T): void {
+    this.onChange(value);
+  }
+
+  @action
+  input(value: T): void {
+    this.onInput(value);
+  }
 }
