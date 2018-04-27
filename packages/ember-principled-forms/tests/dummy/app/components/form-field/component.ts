83aa09165e49c937f71185e6fe25d4a17bd64991
Frank Tan <tansongyang@gmail.com>
2018-04-27 09:08:37 -0400

    Experiment with contextual component

--- packages/ember-principled-forms/tests/dummy/app/components/form-field/component.ts
+++ packages/ember-principled-forms/tests/dummy/app/components/form-field/component.ts
@@ -6,42 +6,45 @@ import update from 'ember-object-update';
 import { Maybe } from 'true-myth';
 
 import Field, { validate } from 'principled-forms/field';
-import Number from 'principled-forms/field/number';
+import EmailField from 'principled-forms/field/email';
+import NumberField from 'principled-forms/field/number';
 import Form, { FromModel } from 'principled-forms/form';
+import Validity from 'principled-forms/validity';
 
 // @ts-ignore: Ignore import of compiled template
 import layout from './template';
 
 type User = {
   age: number;
-  name: Maybe<string>;
+  name: string;
+  email: Maybe<string>;
 };
 
 const userForm: FromModel<User> = user => ({
-  age: Number.required({ eager: false }),
-  name: Field.optional()
+  // DISCUSS: Does it actually every make sense for `eager` to be false?
+  // Isn't our need for eagerness solved by having a separate unvalidated state?
+  age: NumberField.required({ eager: false }),
+  name: Field.required(),
+  email: EmailField.optional({ validity: Validity.valid() }), 
 });
 
 type UserForm = Form<User>;
 type FormProp = keyof UserForm;
+// DISCUSS: This should probably be its own type for convenience, e.g. FormValue<T>
+type FormValue = UserForm[FormProp]['value'];
 
 export default class FormField extends Component {
   layout = layout;
 
-  user: UserForm = userForm({ age: 30, name: Maybe.just('Chris') });
-
-  age = null;
-  name = null;
-
-  foo = this.user.name.value;
+  user: UserForm = userForm({ age: 30, name: 'Chris', email: Maybe.nothing() });
 
   @action
-  handleChange(property: FormProp, value: UserForm[FormProp]['value']) {
-    update(this.user, property, (field: Field<any>) => validate({ ...field, value }));
+  handleChange(property: FormProp, value: FormValue) {
+    update(this.user, property, (field) => validate({ ...field, value }) as any);
   }
 
   @action
-  handleInput(property: FormProp, value: string) {
-    update(this.user, property, (field: Field<any>) => validate({ ...field, value }));
+  handleInput(property: FormProp, value: FormValue) {
+    update(this.user, property, (field) => validate({ ...field, value }) as any);
   }
 }
