803a2b0a46882b6f92d4626e0f481fe4c9ac3140
Chris Krycho <chris@chriskrycho.com>
2018-06-16 15:11:38 -0600

    PF: Add tests for and fix bugs in `validators` module.

--- packages/principled-forms/src/validators.ts
+++ packages/principled-forms/src/validators.ts
@@ -1,27 +1,34 @@
 import { valid, invalid, Validator, Validated } from './validity';
 
-type ErrMessage<T> = (val: T) => string;
+export type ErrMessage<T> = (actual: T) => string;
 
-const defaultInvalid = <T>(fallback: string, requirement: T, err?: ErrMessage<T>) =>
-  invalid(err ? err(requirement) : fallback);
+const defaultInvalid = <T>(fallback: string, actual: T, err?: ErrMessage<T>) =>
+  invalid(err ? err(actual) : fallback);
 
 export const minLength = (min: number, err?: ErrMessage<number>): Validator<string> => (
   value: string
 ): Validated =>
-  value.length >= min ? valid() : defaultInvalid(`Must be at least ${min} characters`, min, err);
+  value.length >= min
+    ? valid()
+    : defaultInvalid(`Must be at least ${min} characters`, value.length, err);
 
 export const maxLength = (max: number, err?: ErrMessage<number>): Validator<string> => (
   value: string
 ): Validated =>
-  value.length <= max ? valid() : defaultInvalid(`Must be at most ${max} characters`, max, err);
+  value.length <= max
+    ? valid()
+    : defaultInvalid(`Must be at most ${max} characters`, value.length, err);
 
 export const minValue = (min: number, err?: ErrMessage<number>): Validator<number> => (
   value: number
-) => (value >= min ? valid() : defaultInvalid(`Must be at least ${min}`, min, err));
+) => (value >= min ? valid() : defaultInvalid(`Must be at least ${min}`, value, err));
 
 export const maxValue = (max: number, err?: ErrMessage<number>): Validator<number> => (
   value: number
-) => (value <= max ? valid() : defaultInvalid(`Must be at most ${max}`, max, err));
+) => (value <= max ? valid() : defaultInvalid(`Must be at most ${max}`, value, err));
 
 export const regex = (re: RegExp, err: ErrMessage<string>): Validator<string> => (value: string) =>
   re.test(value) ? valid() : invalid(err(value));
+
+export const Validators = { minLength, maxLength, minValue, maxValue, regex };
+export default Validators;
